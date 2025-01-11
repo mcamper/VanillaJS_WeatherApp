d0591a75ec4aea85ce02e9b7b76e883ad06ffb5b
mcamper <mariathetech180@gmail.com>
2025-01-11 12:33:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function changeCity() {
+    let city = prompt("What city do you live in?");
+    let temperature = prompt("What temperature is it?");
+    let heading = document.querySelector("h1");
+    if (temperature < 0) {
+        heading.innerHTML = "😔<br />Currently " + temperature + 
+        "°C in " + city;
+    } else {
+        heading.innerHTML = "😊<br />Currently " + temperature + 
+        "°C in " + city; 
+    }
+    }
+
+let changeButton = document.querySelector("button");
+changeButton.addEventListener("click", changeCity);
+
