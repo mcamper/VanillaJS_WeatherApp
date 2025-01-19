4d1c70b87bc891df71aca29a415ae31d272f91f9
mariacamper <mariathetech180@gmail.com>
2025-01-19 17:26:48 -0500

    Add object in js file

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+let weather = {
+    paris: {
+        temp: 19.7,
+        humidity: 80
+    },
+    tokyo: {
+        temp: 17.3,
+        humidity: 50
+    },
+    lisbon: {
+        temp: 30.2,
+        humidity: 20
+    },
+    "san francisco": {
+        temp: 20.9,
+        humidity: 100
+    },
+    oslo: {
+        temp: -5,
+        humidity: 20
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
 
 function changeCity() {
     let city = prompt("What city do you live in?");
